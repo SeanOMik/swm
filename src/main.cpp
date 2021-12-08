e247f107adfe250f93456e05c540d235a5a29ef1
SeanOMik <seanomik@gmail.com>
2021-12-07 20:35:34 -0500

    Add xdgpp (xdg)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 
 #include "config/config.h"
 
+#include "xdg.hpp"
+
 int main() {
+
+#ifndef __DEBUG__
     Config config("config.toml");
+#else
+    std::string path = xdg::ConfigHomeDir();
+    path += "/swm/config.toml";
+    Config config(path);
+#endif
 
     std::cout << "Terminal: \"" << config.programs.terminal << "\" - Launcher: \"" << config.programs.launcher << "\"" << std::endl;
-    
+
     return 0;
 }
